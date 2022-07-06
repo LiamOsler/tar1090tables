54022b77f14bb65e3a9538a913ff34b0c8c10b90
LiamOsler <liam.osler@dal.ca>
2022-07-06 00:47:36 -0300

    move client to within server folder to make routing easier

--- server/Server.js
+++ server/Server.js
@@ -3,27 +3,37 @@
 
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express()
-const port = 8000
+const port = 80
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
 
-const buildPath = path.normalize(path.join(__dirname, '../client/build'));
-app.use(express.static(buildPath));
+app.use('/static', express.static(path.join(__dirname, 'client/build/static')));
+app.use('/manifest.json', express.static(path.join(__dirname, 'client/build/manifest.json')));
 
-const clientURL = 'client/build/index.html';
+const clientURL = '/client/build/index.html';
+
+async function getData() {
+  const response = await axios.get('http://192.168.2.36/tar1090/data/aircraft.json');
+  return response.data;
+}
+
 app.get('/tar1090tables', (req, res) => {
-  res.sendFile(clientURL, {'root': '../'});
+  res.sendFile(clientURL, {'root': './'});
 })
 
 app.get('/tar1090tables/data', (req, res) => {
-  res.json({"message": "hello"});
+  getData().then(
+    (data) => {
+      res.json(data);
+    }
+  )
 })
 
-function fetchData(){
-  console.log("fetching data");
-}
 
+
+// console.log(test);
